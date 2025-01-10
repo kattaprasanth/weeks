37f2c5abaeb36362460d973967c9b03ac7640213
Prasanth katta <127kattaprasanth@gmail.com>
2025-01-10 14:43:56 +0000

    added array of weeks range to be returned

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from datetime import date, timedelta
 import unittest
 
-def get_previous_week_range(weeks: int):
+def get_previous_week_range(weeks):
     if weeks <= 0:
         raise Exception("weeks should not be less than or equal to 0")
     current_week = 1
     total_weeks = current_week + weeks
     previous_date = date.today() - timedelta(days=7*total_weeks)
     result = previous_date.isocalendar()
+    output = []
+    for i in range(weeks):
+        output.append(f"{result.year}-{result.week + i}")
+    print(output)
     return f"{result.year}-{result.week}"
 
 
@@ -38,4 +42,10 @@ class TestPreviousWeeksRange(unittest.TestCase):
         self.assertEqual(expected, got)
 
 if __name__ == '__main__':
-    unittest.main()
+    # unittest.main()
+    get_previous_week_range(1)
+    get_previous_week_range(2)
+    get_previous_week_range(3)
+    get_previous_week_range(10)
+    get_previous_week_range(52)
+    # get_previous_week_range(100)
